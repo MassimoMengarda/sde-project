1b56409b4daf052efd7b5bffe768025921de3ba6
Massimo Mengarda <max.mengarda@gmail.com>
2020-12-11 21:06:35 +0100

    Finish Italy adapter and small refactor

--- adapters/italy_adapter.js
+++ adapters/italy_adapter.js
@@ -1,63 +1,51 @@
 const fetch = require('node-fetch');
-const parse = require('csv-parse/lib/sync');
 
 const BASE_URL = 'https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-regioni/dpc-covid19-ita-regioni.csv';
 
 const getData = async (req, res) => {
-  var result = [];
-  console.log("Starting fetching Italy dataset...");
-  const data = await fetch(BASE_URL)
-    .then((fetch_res) => {
-      const csv_data = fetch_res.text();
-      return csv_data;
-    })
-    .then((csv) => {
-      var lines = csv.toString().split("\n");
-      var headers = lines[0].split(",");
-      for (var i = 1; i < lines.length; i++) {
-        var obj = {};
-        var currentline = lines[i].split(",");
+    console.log('Starting fetching Italy dataset...');
+    const data = await fetch(BASE_URL).then(fetch_res => {
+        return fetch_res.text();
+    }).then(resCSV => {
+        console.log('Done fetching\nElaborating...');
+        let result = [];
+        const lines = resCSV.toString().split('\n');
+        if (lines.length > 0) {
+            const headers = lines[0].split(',');
+            for (const line of lines) {
+                const obj = {};
+                const fields = line.split(',');
 
-        for (var j = 0; j < headers.length; j++) {
-          obj[headers[j]] = currentline[j];
+                for (let i = 0; i < headers.length; i++) {
+                    obj[headers[i]] = fields[i];
+                }
+                result.push(obj);
+            }
         }
-
-        result.push(obj);
-      }
-      return { result: result };
+        return { result: filter(result) };
     });
-  //console.log(data);
-  // .then(resJSON => {
-  //     console.log('Done fetching\nElaborating...');
-  //     const result = filter(resJSON);
-  //     return {result: result};
-  // });
 
-  console.log("Sending data...");
-  res.status(200);
-  res.send(data);
-  console.log("Done!");
+    console.log('Sending data...');
+    res.status(200);
+    res.send(data);
+    console.log('Done!');
 };
 
 function filter(data) {
     const result = {};
 
+    data.shift(); // Skip header
     for (const elem of data) {
-        const date = elem.DATE;
-        const province = elem.PROVINCE;
-        let cases = elem.CASES;
+        let date = elem.data;
+        const province = elem.denominazione_regione;
+        const cases = elem.nuovi_positivi;
 
         if (date !== undefined && province !== undefined && cases !== undefined) {
+            date = date.substring(0, 10);
             if (result[date] === undefined) {
                 result[date] = {};
             }
-            if (result[date][province] === undefined) {
-                result[date][province] = {cases: 0};
-            }
-            if (cases === '<5') {
-                cases = Math.round(Math.random() * 5);
-            }
-            result[date][province].cases += parseInt(cases);
+            result[date][province] = {cases: cases};
         }
     }
     return result;
