183b8ef2f944f0019025774d3789f268e4966329
alexzanetti <alex.zanetti-1@studenti.unitn.it>
2021-01-22 11:52:41 +0100

    Initial set up of country details part

--- frontend/src/app/country-details/country-details.component.ts
+++ frontend/src/app/country-details/country-details.component.ts
@@ -1,13 +1,24 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnChanges } from '@angular/core';
 
 @Component({
   selector: 'app-country-details',
   templateUrl: './country-details.component.html',
   styleUrls: ['./country-details.component.scss']
 })
-export class CountryDetailsComponent {
+export class CountryDetailsComponent implements OnChanges{
   @Input() country: string;
+  public selectedCountry: string = '';
 
   public constructor() { }
+
+  public ngOnChanges(): void {
+    if (this.country === 'IT')
+      this.selectedCountry = 'Italy';
+    else if (this.country === 'BG')
+      this.selectedCountry = 'Belgium';
+    else if (this.country === 'UK')
+      this.selectedCountry = 'United Kingdom';
+  }
+
 
 }
