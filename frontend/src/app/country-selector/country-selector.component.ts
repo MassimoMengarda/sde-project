39266db6cb2131d0731f233329f8a97d01091f76
alexzanetti <alex.zanetti-1@studenti.unitn.it>
2021-02-15 10:51:47 +0100

    Loading GIF added and print last-24-h cases per country

--- frontend/src/app/country-selector/country-selector.component.ts
+++ frontend/src/app/country-selector/country-selector.component.ts
@@ -1,15 +1,27 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, Input, OnInit } from '@angular/core';
+import { CovidDataService } from 'src/app/shared/services/covid-data.service';
 
 @Component({
   selector: 'app-country-selector',
   templateUrl: './country-selector.component.html',
   styleUrls: ['./country-selector.component.scss'],
 })
-export class CountrySelectorComponent {
+export class CountrySelectorComponent implements OnInit{
   public currentCountry: string;
   public isDisabled: boolean;
+  public isInfoLoaded = false;
+  public allData: any;
 
-  constructor() {}
+  public constructor(private CovidSvc: CovidDataService) {}
+
+  public ngOnInit(): void {
+    // TODO: create an endpoint to get only cases values and not all dataset
+    this.CovidSvc.getAllDataset().subscribe((res) => {
+      this.isInfoLoaded = true;
+      this.allData = res;
+    });
+  }
 
   public setCurrentCountry(country: string): void {
     this.currentCountry = country;
