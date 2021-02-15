fb8dc97730d6ab32fa382fbe49c1dbfb797be5bb
alexzanetti <alex.zanetti-1@studenti.unitn.it>
2021-02-15 10:41:12 +0100

    Small refactor

--- frontend/src/app/country-details/map/map.component.ts
+++ frontend/src/app/country-details/map/map.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
-  styleUrls: ['../country-details.component.scss', './map.component.scss']
+  styleUrls: ['./map.component.scss']
 })
 export class MapComponent implements OnInit {
 
