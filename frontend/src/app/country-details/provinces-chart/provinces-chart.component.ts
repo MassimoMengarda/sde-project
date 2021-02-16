831fd04a76f96c19997dcaf1fdc069fd1923a996
alexzanetti <alex.zanetti-1@studenti.unitn.it>
2021-02-16 10:04:37 +0100

    Provinces cases now update automatically on date range change

--- frontend/src/app/country-details/provinces-chart/provinces-chart.component.ts
+++ frontend/src/app/country-details/provinces-chart/provinces-chart.component.ts
@@ -1,6 +1,7 @@
 import { DatePipe } from '@angular/common';
 import { Component, Input, OnChanges, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
+import { MatDatepickerInputEvent } from '@angular/material/datepicker';
 import { DatesMapperService } from 'src/app/shared/services/dates-mapper.service';
 
 @Component({
@@ -14,7 +15,8 @@ export class ProvincesChartComponent implements OnChanges {
   public provincesBox = new Map<string, number>();
   public provincesEntries;
 
-  public provincesName:string[] = [];
+  private startDate: string;
+  private endDate: string;
 
   public constructor(private datesMapperSvc: DatesMapperService, private datePipe: DatePipe) {
     const today = new Date();
@@ -29,20 +31,35 @@ export class ProvincesChartComponent implements OnChanges {
   }
 
   public ngOnChanges(): void {
-    const startDate = this.datePipe.transform(this.range.value.start, 'yyyy-MM-dd');
-    const endDate = this.datePipe.transform(this.range.value.end, 'yyyy-MM-dd');
+    this.startDate = this.datePipe.transform(this.range.value.start, 'yyyy-MM-dd');
+    this.endDate = this.datePipe.transform(this.range.value.end, 'yyyy-MM-dd');
 
     //Svuoto nel caso ci siano dentro le vecchie entries di altri stati
     this.provincesBox.clear();
 
+    this.selectCountry();
+  }
+
+  private selectCountry() {
     if (this.country === 'Italy') {
-      this.getItalyCasesProvinces(startDate, endDate);
+      this.getItalyCasesProvinces(this.startDate, this.endDate);
     } else if (this.country === 'Belgium') {
-      this.getBelgiumCasesProvinces(startDate, endDate);
+      this.getBelgiumCasesProvinces(this.startDate, this.endDate);
     } else {
-      this.getUKCasesProvinces(startDate, endDate);
+      this.getUKCasesProvinces(this.startDate, this.endDate);
     }
   }
+
+  public startChange(event): void {
+   this.startDate = this.datePipe.transform(this.range.value.start, 'yyyy-MM-dd');
+  }
+  public endChange(event): void {
+   if (event.value) {
+     this.endDate = this.datePipe.transform(this.range.value.end, 'yyyy-MM-dd');
+     this.selectCountry();
+   }
+  }
+
 
   private getItalyCasesProvinces(startDate: string, endDate: string) {
     this.datesMapperSvc.getItalyData(startDate, endDate).subscribe(res => {
