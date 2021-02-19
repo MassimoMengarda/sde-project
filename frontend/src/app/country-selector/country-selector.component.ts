e1d9fa85caeedf6b9f2420b463441c9b2eb407db
alexzanetti <alex.zanetti-1@studenti.unitn.it>
2021-02-19 12:08:11 +0100

    Fix latest cases available in country selector

--- frontend/src/app/country-selector/country-selector.component.ts
+++ frontend/src/app/country-selector/country-selector.component.ts
@@ -10,9 +10,9 @@ export class CountrySelectorComponent implements OnInit {
   public currentCountry: string;
   public isInfoLoaded = false;
 
-  public italyCases: any;
-  public belgiumCases: number;
-  public UKCases: number;
+  public latestItalyData: any;
+  public latestBelgiumData: number;
+  public latestUKData: number;
 
   public constructor(private regionMapperSvc: RegionMapperService) {}
 
@@ -20,12 +20,12 @@ export class CountrySelectorComponent implements OnInit {
     this.getLast24hCases();
   }
 
-  // Function to gets the latest available cases
+  // Function to gets the latest available data
   private getLast24hCases() {
     this.regionMapperSvc.getCases().subscribe((res) => {
-      this.italyCases = res[0].italy[0].cases;
-      this.belgiumCases = res[1].belgium[0].cases;
-      this.UKCases = res[2].uk[0].cases;
+      this.latestItalyData = res[0].italy[0];
+      this.latestBelgiumData = res[1].belgium[0];
+      this.latestUKData = res[2].uk[0];
       this.isInfoLoaded = true;
     });
   }
